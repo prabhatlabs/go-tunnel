2d8df69c2f283822ca99291ce55852fcc5d02177
Prabhat <workforprabhat1254@gmail.com>
2026-06-25 18:51:59 +0530

    removed: fatel error on .env not found

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,9 +10,7 @@ import (
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	godotenv.Load()
 
 	port := os.Getenv("PORT")
 	secret := os.Getenv("SECRET")
